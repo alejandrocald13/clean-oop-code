4fc6c2efd0763b9c113bad2eb54bb4fb42066972
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-08-18 22:29:45 -0600

    feat(db): add database connection

--- my-posts-app/src/app/api/route.ts
+++ my-posts-app/src/app/api/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
+import postgres from "postgres";
 
 export async function POST(request: NextRequest){
-    const data = await request.json();
+
+    try {
+        const connectionString = 'postgresql://postgres:PW0tzIjvPIo3TcXn@db.fvsvtllszvjinglgnyxt.supabase.co:5432/postgres'
+        const sql = postgres(connectionString)
+
+        const data = await request.json();
     
     if (data && data.id && data.title && data.description && data.author) {
         let postApproved = true
@@ -23,15 +29,24 @@ export async function POST(request: NextRequest){
         }
 
         if (postApproved) {
+
+            // add logic to save data
+
             return NextResponse.json({message: 'Post is valid', post: data});
+
         } else {
             return NextResponse.json({
                 error: 'Invalid post format'
             }, {status: 422});
         }
 
+        }
+
+    } catch (error) {
+        console.error('Error saving post', error)
+        return NextResponse.json({
+            error: 'Failed to save post'},
+            {status: 500})
     }
-    return NextResponse.json({
-        data
-    })
+    
 }
